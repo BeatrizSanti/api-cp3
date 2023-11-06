c125e5fde52d56dd86c2d6ebfc1890962ce40a06
aaaaaaaaana <rm551401@fiap.com.br>
2023-11-06 19:13:46 -0300

    arrmando api

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -5,9 +5,8 @@ export default function Cabecalho() {
     return (
         <div>
             <header className="bg-teal-900 text-white h-16">
-
                 <nav className="flex flex-row justify-around justify-items-center">
-                    <Link className="link-cabecalho" href="/consumo/eletro-view">Eletrônicos</Link>
+                    <Link className="link-cabecalho" href="/consumo/eletro-view">eletrônicos</Link>
                     <Link className="link-cabecalho" href="/produtos/produto/estaticas/tenis">tenis</Link>
                     <Link className="link-cabecalho" href="/produtos/produto/dinamicas/estranho/tenis-preto">tenis preto</Link>
                     <Link className="link-cabecalho" href="/produtos/produto/dinamicas/vestuario/camisa-xadrez">camisa-x</Link>
