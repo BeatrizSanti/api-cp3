fc54d6c0a899eeb06b611810b9c94c65a1c893eb
BeatrizSanti <rm551856@fiap.com.br>
2023-11-06 17:29:20 -0300

    Cabecalho e Rodape

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
 
+
 export default function Cabecalho() {
-  return (
+    return (
         <header className="bg-gray-800 text-white h-16">
-          <nav className="flex flex-row justify-around">
-            <Link className="link-cabecalho" href="/consumo/eletro-view">ELETRÔNICOS</Link>
-            <Link className="link-cabecalho" href="/produtos/estaticas/tenis">TÊNIS</Link>
-            <Link className="link-cabecalho" href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
-            <Link className="link-cabecalho" href="/produtos/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
-          </nav>
+            <nav className="flex flex-row justify-around">
+                <Link className="link-cabecalho" href="/consumo/eletro-view">Eletrônicos</Link>
+                <Link className="link-cabecalho" href="/produtos/produto/estaticas/tenis">TÊNIS</Link>
+                <Link className="link-cabecalho" href="/produtos/produto/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
+                <Link className="link-cabecalho" href="/produtos/produto/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
+                <Link className="link-cabecalho" href="/consumo/user-view">Usuários</Link>
+            </nav>
         </header>
-  )
+    )
 }
--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-
 export default function Rodape() {
   return (
-    <footer className="rodape">
-        <div>
-            <p>Desenvolvido por: <span>pf0670</span></p>
-            <p>NEXT.js - React.js - GitHub - tailwindcss</p>
-        </div>
-    </footer> 
+      <footer className="rodape">
+          <div>
+              <p>Desenvolvido por: <span>rm551856</span></p>
+              <p>NEXT.js - React.js - Git-Hub - tailwindcss</p>
+          </div>
+      </footer>
   )
 }
- 
