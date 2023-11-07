2397a5a4b769a923c94ace12db7e9a32689d42e6
M1relly <rm98672@fiap.com.br>
2023-11-07 08:31:28 -0300

    commit com o professor

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Cabecalho() {
     return (
         <div>
-            <header className="bg-teal-900 text-white h-16">
+            <header className="bg-teal-900 text-white h-16 ">
                 <nav className="flex flex-row justify-around justify-items-center">
                     <Link className="link-cabecalho" href="/consumo/eletro-view">Eletrônicos</Link>
                     <Link className="link-cabecalho" href="/produtos/produto/estaticas/tenis">Tenis</Link>
